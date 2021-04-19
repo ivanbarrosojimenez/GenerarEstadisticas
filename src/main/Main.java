52165a6818b99e9e235dccce6fa40387d69d3b47
Carlos Benavent Olmo <carlos.benavent@inetum.world>
2021-04-19 09:46:57 +0200

    Cambio sobre main

--- src/main/Main.java
+++ src/main/Main.java
@@ -48,7 +48,7 @@ public class Main {
 //    static String NOMBRE_FICHERO_ENTRADA_2_5 = "host6.json";
     /** * * * * * * * * * * * * * * * * * * * * * * * * * * */
     /** - - - - - MODIFICAR SOLO ESTO - - - - -*/
-	static int numFasesCrear = 4;//Si se quiere ejecutar solo una fase cambiar tambien donde pone carpeta con el mismo numero (L67)
+	static int numFasesCrear = 6;//Si se quiere ejecutar solo una fase cambiar tambien donde pone carpeta con el mismo numero (L67)
 	static String tipoResultado = "Db2"; //poner SQL o Db2;
 	static boolean comprimirYEliminar = false;
 	static String tipo = "resultados";//tiempos o resultados
@@ -68,7 +68,7 @@ public class Main {
         grabarFichero.crearFichero("salida/"+tipo + NOMBRE_FICHERO_SALIDA_FILTRADO, true);
 
 
-        for(int fase = 0, carpeta = 1; carpeta <= numFasesCrear; fase++,carpeta++) {
+        for(int fase = 0, carpeta = 4; carpeta <= numFasesCrear; fase++,carpeta++) {
         	
         	String nombreCarpetaFase ="F"+carpeta+"resultados"+tipoResultado; 
         	File files[] = (new File(nombreCarpetaFase+"/")).listFiles(filtro);
